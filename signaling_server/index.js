143c358a73797766294587de423368616bf7ee5f
Dragmerz <152835014+Dragmerz@users.noreply.github.com>
2026-02-21 10:51:39 +0530

    Remove CORS middleware from the server

--- signaling_server/index.js
+++ signaling_server/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 const SECRET = "super_secret_walkie_key"; // change later
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
